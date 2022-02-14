a3cc26d68a11c67a57401a8246b688ee24e0653a
TheOddSheep <buttyboy63@gmail.com>
2022-02-14 17:32:48 +0100

    Loading docker images into k8s

--- load_images.sh
+++ load_images.sh
@@ -15,4 +15,17 @@ docker build -t "reviewer_ratings:$version" ./src/ratings/
 
 kind load docker-image reviewer_mongodb:$version
 echo "${green}mongodb image has been loaded in kind ! ${reset}" 
+
 #reviewer_details:$version reviewer_mysql:$version reviewer_productpage:$version reviewer_ratings:$version
+
+kind load docker-image reviewer_mysql:$version
+echo "${green}mysql image has been loaded in kind ! ${reset}"
+
+kind load docker-image reviewer_productpage:$version
+echo "${green}productpage image has been loaded in kind ! ${reset}"
+
+kind load docker-image reviewer_details:$version
+echo "${green}details image has been loaded in kind ! ${reset}"
+
+kind load docker-image reviewer_ratings:$version
+echo "${green}ratings image has been loaded in kind ! ${reset}"
